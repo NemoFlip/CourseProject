cb493ac2f0c140fe41bec9dc589c478461a8649f
Voldemar Bauer <voldemarbauer3930@gmail.com>
2024-11-07 00:52:13 +0300

    add: login message

--- cmd/src/login.cpp
+++ cmd/src/login.cpp
@@ -1,2 +1,13 @@
-#include "../include/api/Navigator.hpp"
+#include "../include/login/login.hpp"
+#include "../include/servisesURIs.hpp"
+#include <drogon/HttpClient.h>
 
+void login(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {  
+  std::string loginURL = servisesURI::auth_service;
+
+  auto client = drogon::HttpClient::newHttpClient(loginURL);
+
+  auto nextReq = drogon::HttpRequest::newHttpRequest();
+  nextReq->setMethod(req->getMethod());
+  nextReq->setPath("/login");
+}
