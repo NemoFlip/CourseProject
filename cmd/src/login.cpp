7b8d01ab0765afb0d4ff6d118bee34aec09c09aa
Voldemar Bauer <voldemarbauer3930@gmail.com>
2024-11-07 00:54:55 +0300

    add: request to login endpoint

--- cmd/src/login.cpp
+++ cmd/src/login.cpp
@@ -2,12 +2,29 @@
 #include "../include/servisesURIs.hpp"
 #include <drogon/HttpClient.h>
 
+drogon::HttpRequestPtr createNewRequest(const drogon::HttpRequestPtr& req, const std::string& path) {
+  auto newReq = drogon::HttpRequest::newHttpRequest();
+
+  newReq->setMethod(req->getMethod());
+  newReq->setPath(path);
+  newReq->setBody(std::string(req->getBody()));
+  
+  return std::move(newReq);
+}
+
 void login(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {  
-  std::string loginURL = servisesURI::auth_service;
+  auto client = drogon::HttpClient::newHttpClient(servisesURI::auth_service);
+  auto newReq = createNewRequest(req, "/login");
 
-  auto client = drogon::HttpClient::newHttpClient(loginURL);
-
-  auto nextReq = drogon::HttpRequest::newHttpRequest();
-  nextReq->setMethod(req->getMethod());
-  nextReq->setPath("/login");
+  client->sendRequest(
+    newReq, [callback = std::move(callback)](drogon::ReqResult result,const drogon::HttpResponsePtr& resp) {
+      if (result == drogon::ReqResult::Ok) {
+        //add some logs later
+        callback(resp);
+      }
+      else {
+        //add some logs
+        std::cerr << result << '\n';
+      }
+    });
 }
