bb75a91410dbaa8aff1840103eaabb6bc6d5cabb
Voldemar Bauer <voldemarbauer3930@gmail.com>
2024-11-07 00:53:30 +0300

    add: front_server path

---
+++ cmd/include/servisesURIs.hpp
@@ -0,0 +1,12 @@
+
+#ifndef SERVISESURIS_HPP
+#define SERVISESURIS_HPP
+
+namespace servisesURI {
+  const std::string auth_service = "";
+  const std::string profile_service = "";
+  const std::string courses_servise = "";
+  const std::string front = "";
+}
+
+#endif // !SERVISESURIS_HPP
