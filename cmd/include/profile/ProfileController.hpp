78e8fd7c12eb002e9ff78cb9fa32a28d112b1b22
Voldemar Bauer <voldemarbauer3930@gmail.com>
2024-11-07 19:19:32 +0300

    add: ProfileController

---
+++ cmd/include/profile/ProfileController.hpp
@@ -0,0 +1,23 @@
+
+#ifndef PROFILECONTROLLER_HPP
+#define PROFILECONTROLLER_HPP
+
+#include <drogon/drogon.h>
+#include <drogon/HttpController.h>
+
+
+class ProfileController : public drogon::HttpController<ProfileController> {
+
+public:
+  METHOD_LIST_BEGIN
+
+
+  METHOD_LIST_END
+
+protected:
+
+};
+
+#endif // !PROFILECONTROLLER_HPP
+
+
