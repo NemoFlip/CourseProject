59a4709b2db94f1b5b0d97f71b0290abfe5e5652
Voldemar Bauer <voldemarbauer3930@gmail.com>
2024-11-12 15:40:42 +0300

    add: logs and calculating requests time

--- cmd/src/LoginController.cpp
+++ cmd/src/LoginController.cpp
@@ -1,6 +1,7 @@
-#include "../include/login/login.hpp"
+#include "../include/login/LoginController.hpp"
 #include "../include/servisesURIs.hpp"
 #include <drogon/HttpClient.h>
+#include <chrono>
 
 void resendRequest(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, const std::string& path) 
 {
@@ -11,16 +12,49 @@ void resendRequest(const drogon::HttpRequestPtr& req, std::function<void(const d
   newReq->setPath(path);
   newReq->setBody(std::string(req->getBody()));
 
+  LOG->trace("Sending request to the {} adress to the {} endpoint.", servisesURI::auth_service, path);
+
+  auto start = std::chrono::high_resolution_clock::now();
   client->sendRequest(
-    newReq, [callback = std::move(callback)](drogon::ReqResult result,const drogon::HttpResponsePtr& resp) {
+    newReq, [callback = std::move(callback), &start](drogon::ReqResult result,const drogon::HttpResponsePtr& resp) {
+
+      auto end = std::chrono::high_resolution_clock::now();
+      auto duration = std::chrono::duration<float>(end - start);
+      
       if (result == drogon::ReqResult::Ok) {
-        //add some logs later
-        callback(resp);
+        LOG->trace("Received a response with {} status codó.", resp->getStatusCode());
+        LOG->info("Ñonnection succeeded.");
       }
       else {
-        //add some logs
-        std::cerr << result << '\n';
+        switch (result) {
+        case drogon::ReqResult::BadResponse:
+          LOG->error("Receives a badresponse from server.");
+          break;
+        case drogon::ReqResult::NetworkFailure:
+          LOG->error("Trouble with the Network.");
+          break;
+        case drogon::ReqResult::BadServerAddress:
+          LOG->error("Bad server address specified.");
+          break;
+        case drogon::ReqResult::Timeout:
+          LOG->error("Request timed out.");
+          break;
+        case drogon::ReqResult::HandshakeError:
+          LOG->error("TLS handshake error occurred.");
+          break;
+        case drogon::ReqResult::InvalidCertificate:
+          LOG->error("The invalid certificate from server.");
+          break;
+        case drogon::ReqResult::EncryptionFailure:
+          LOG->error("The encryption failed.");
+          break;
+        default:
+          break;
+        }
+        LOG->info("Connection failure.");
       }
+      LOG->info("Request time: {} ms", duration);
+      callback(resp);
     });
 }
 
