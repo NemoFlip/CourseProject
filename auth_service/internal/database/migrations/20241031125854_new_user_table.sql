f94070299c5296b94a81b22a29c72f4f78338e98
NemoFlip <artgamer3163@gmail.com>
2024-11-12 08:02:26 +0300

    made: first migration with users table

---
+++ auth_service/internal/database/migrations/20241031125854_new_user_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS users(
+    id VARCHAR(36) PRIMARY KEY NOT NULL,
+    username VARCHAR(25) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(18) NULL DEFAULT NULL,
+    password VARCHAR(25) NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
