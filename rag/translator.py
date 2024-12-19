c12e37f5cb20f8cbaf1786dbed38e3f9e3ee7755
Pavel Durinin <145273332+pavel625@users.noreply.github.com>
2024-12-20 00:05:44 +0300

    Update translator.py

--- rag/translator.py
+++ rag/translator.py
@@ -1,8 +1,6 @@
 from deep_translator import GoogleTranslator
 
 class Translator:
-    """Класс для перевода текста с использованием GoogleTranslator."""
     @staticmethod
     def translate(text, src_lang="auto", dest_lang="en"):
-        """Перевод текста с одного языка на другой."""
         return GoogleTranslator(source=src_lang, target=dest_lang).translate(text)
