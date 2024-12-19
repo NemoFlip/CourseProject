ac9bbd30af03cb0a38f57fbd9931b011495a54fb
Pavel Durinin <145273332+pavel625@users.noreply.github.com>
2024-12-20 00:05:06 +0300

    qa_service.py

--- rag/qa_service.py
+++ rag/qa_service.py
@@ -3,13 +3,11 @@ from langchain.callbacks import get_openai_callback
 from langchain.chains.question_answering import load_qa_chain
 
 class QAService:
-    """Класс для ответа на вопросы с использованием LangChain."""
     def __init__(self):
         self.llm = OpenAI(temperature=0)
         self.chain = load_qa_chain(self.llm, chain_type="refine")
 
     def get_answer(self, documents, question):
-        """Возвращает ответ на вопрос на основе документов."""
         with get_openai_callback():
             response = self.chain.run(input_documents=documents, question=question)
         return response
