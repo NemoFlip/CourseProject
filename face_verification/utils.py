1b40361ed0308ad584fcd950cead2d8126d20900
Pavel Durinin <145273332+pavel625@users.noreply.github.com>
2024-12-22 20:51:37 +0300

    Delete face_verification directory

--- face_verification/utils.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-def capture_face_image():
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    cap.release()
-    if not ret:
-        raise Exception("Ошибка при захвате изображения с камеры")
-    return frame
