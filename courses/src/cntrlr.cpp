e7d3f1537fc680ed4d35af5aa9c01065302fe605
Voldemar Bauer <voldemarbauer3930@gmail.com>
2024-12-29 12:25:31 +0300

    upd: code structere

--- courses/src/cntrlr.cpp
+++ courses/src/cntrlr.cpp
@@ -7,7 +7,46 @@ enum class CourseStates{
   LeaveCourse
 };
 
-void CourseParticipation(const CourseStates& State, const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+void sendJoinCourseQuery(Json::Value& response_body, const std::string& user_id, const std::string& course_id,
+  std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+  Database::getInstance().insert_query("courses_participation",
+    "user_id, course_id",
+    "?,?",
+    [callback = std::forward<decltype(callback)>(callback)](const drogon::orm::Result& result) {
+      auto resp = drogon::HttpResponse::newHttpResponse();
+      resp->setStatusCode(drogon::k204NoContent);
+      callback(resp);
+    },
+    [callback = std::forward<decltype(callback)>(callback), &response_body](const drogon::orm::DrogonDbException& exc) {
+      response_body["Error"] = exc.base().what();
+      auto resp = drogon::HttpResponse::newHttpJsonResponse(response_body);
+      resp->setStatusCode(drogon::k500InternalServerError);
+      callback(resp);
+    },
+    user_id,
+    course_id);
+}
+
+void sendLeaveCourseQuery(Json::Value& response_body, const std::string& user_id, const std::string& course_id,
+  std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+  Database::getInstance().delete_query("courses_participation",
+    "user_id = ? and course_id = ?",
+    [callback = std::forward<decltype(callback)>(callback)](const drogon::orm::Result& result) {
+      auto resp = drogon::HttpResponse::newHttpResponse();
+      resp->setStatusCode(drogon::k204NoContent);
+      callback(resp);
+    },
+    [callback = std::forward<decltype(callback)>(callback), &response_body](const drogon::orm::DrogonDbException& exc) {
+      response_body["Error"] = exc.base().what();
+      auto resp = drogon::HttpResponse::newHttpJsonResponse(response_body);
+      resp->setStatusCode(drogon::k500InternalServerError);
+      callback(resp);
+    },
+    user_id,
+    course_id);
+}
+
+void courseParticipation(const CourseStates& State, const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
   auto body = req->getJsonObject();
 
   if (body->empty() || !body->isMember("course_id") || !body->isMember("user_id") ||
@@ -15,73 +54,48 @@ void CourseParticipation(const CourseStates& State, const drogon::HttpRequestPtr
     auto resp = drogon::HttpResponse::newHttpJsonResponse({ {"Error", "Invalid request"} });
     resp->setStatusCode(drogon::k400BadRequest);
     callback(resp);
+    return;
   }
-  else {
-    const std::string course_id = (*body)["course_id"].asString();
-    const std::string user_id = (*body)["user_id"].asString();
+  const std::string course_id = (*body)["course_id"].asString();
+  const std::string user_id = (*body)["user_id"].asString();
 
-    Json::Value response_body;
-    if (State == CourseStates::JoinCourse) {
-      Database::getInstance().insert_query("courses_participation",
-        "user_id, course_id",
-        "?,?",
-        [callback = std::move(callback)](const drogon::orm::Result& result) {
-          auto resp = drogon::HttpResponse::newHttpResponse();
-          resp->setStatusCode(drogon::k204NoContent);
-          callback(resp);
-        },
-        [callback = std::move(callback), &response_body](const drogon::orm::DrogonDbException& exc) {
-          response_body["Error"] = exc.base().what();
-          auto resp = drogon::HttpResponse::newHttpJsonResponse(response_body);
-          resp->setStatusCode(drogon::k500InternalServerError);
-          callback(resp);
-        },
-        user_id,
-        course_id);
-    }
-    else {
-      Database::getInstance().delete_query("courses_participation",
-        "user_id = ? and course_id = ?",
-        [callback = std::forward<decltype(callback)>(callback)](const drogon::orm::Result& result) {
-          auto resp = drogon::HttpResponse::newHttpResponse();
-          resp->setStatusCode(drogon::k204NoContent);
-          callback(resp);
-        },
-        [callback = std::forward<decltype(callback)>(callback), &response_body](const drogon::orm::DrogonDbException& exc) {
-          response_body["Error"] = exc.base().what();
-          auto resp = drogon::HttpResponse::newHttpJsonResponse(response_body);
-          resp->setStatusCode(drogon::k500InternalServerError);
-          callback(resp);
-        },
-        user_id,
-        course_id);
-    }
+  Json::Value response_body;
+  switch (State)
+  {
+  case(CourseStates::JoinCourse):
+    sendJoinCourseQuery(response_body, user_id, course_id, std::forward<decltype(callback)>(callback));
+    break;
+  case(CourseStates::LeaveCourse):
+    sendLeaveCourseQuery(response_body, user_id, course_id, std::forward<decltype(callback)>(callback));
+    break;
+  default:
+    break;
   }
 }
 
-void Cntrlr::get_courses(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+void Cntrlr::getCourses(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
   Json::Value response_body;
 
   Database::getInstance().select_query("courses", basic_values::condition, basic_values::sorting,
     [callback = std::forward<decltype(callback)>(callback), &response_body](const drogon::orm::Result& res) {
       if (res.empty()) {
         response_body["Error"] = "No available courses";
+        return;
       }
-      else {
-        for (const auto& rows : res) {
-          Json::Value tmp;
-          
-          for (const auto& column : rows) {
-            if (!column.isNull()) {
-              tmp[column.name()] = column.as<std::string>();
-            }
-            else {
-              tmp[column.name()] = "Null";
-            }
-          }
 
-          response_body.append(tmp);
+      for (const auto& rows : res) {
+        Json::Value tmp;
+          
+        for (const auto& column : rows) {
+          if (!column.isNull()) {
+            tmp[column.name()] = column.as<std::string>();
+          }
+          else {
+            tmp[column.name()] = "Null";
+          }
         }
+
+        response_body.append(tmp);
       }
       auto resp = drogon::HttpResponse::newHttpJsonResponse(response_body);
       callback(resp);
@@ -94,14 +108,18 @@ void Cntrlr::get_courses(const drogon::HttpRequestPtr& req, std::function<void(c
     });
 }
 
-void Cntrlr::join_course(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
-  CourseParticipation(CourseStates::JoinCourse, req, std::forward<decltype(callback)>(callback));
+void Cntrlr::joinCourse(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+  courseParticipation(CourseStates::JoinCourse, req, std::forward<decltype(callback)>(callback));
 }
 
-void Cntrlr::leave_course(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
-  CourseParticipation(CourseStates::LeaveCourse, req, std::forward<decltype(callback)>(callback));
+void Cntrlr::leaveCourse(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+  courseParticipation(CourseStates::LeaveCourse, req, std::forward<decltype(callback)>(callback));
 }
 
-void Cntrlr::assess_course(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+void Cntrlr::rateCourse(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+
+}
+
+void Cntrlr::changeCourseRating(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
 
 }
