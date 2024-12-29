8f5724f86b64c9b0ea809af0dd380677db96e19e
Voldemar Bauer <voldemarbauer3930@gmail.com>
2024-12-29 12:24:37 +0300

    add: changingCourseRating endpoint

--- courses/include/cntrlr/cntrlr.hpp
+++ courses/include/cntrlr/cntrlr.hpp
@@ -6,16 +6,19 @@ class Cntrlr : public drogon::HttpController<Cntrlr> {
 public:
   METHOD_LIST_BEGIN
 
-  METHOD_ADD(Cntrlr::get_courses, "/courses/list", drogon::Get);
-  METHOD_ADD(Cntrlr::join_course, "/courses/join_course", drogon::Post);
-  METHOD_ADD(Cntrlr::leave_course, "/courses/leave_course", drogon::Delete);
-  METHOD_ADD(Cntrlr::assess_course, "/courses/assess_course", drogon::Put);
+  METHOD_ADD(Cntrlr::getCourses, "/courses/list", drogon::Get);
+  METHOD_ADD(Cntrlr::joinCourse, "/courses/join_course", drogon::Post);
+  METHOD_ADD(Cntrlr::leaveCourse, "/courses/leave_course", drogon::Delete);
+  METHOD_ADD(Cntrlr::rateCourse, "/courses/rate_course", drogon::Post);
+  METHOD_ADD(Cntrlr::changeCourseRating, "/courses/change_rating", drogon::Put);
 
   METHOD_LIST_END
 protected:
 
-  void get_courses(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
-  void join_course(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
-  void leave_course(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
-  void assess_course(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
+  void getCourses(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
+  void joinCourse(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
+  void leaveCourse(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
+  void rateCourse(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
+  void changeCourseRating(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
+
 };
