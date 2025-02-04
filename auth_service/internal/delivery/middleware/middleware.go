d9ad046050211e972770ffb1d461ad0680e211ca
NemoFlip <artgamer3163@gmail.com>
2025-02-05 00:14:15 +0300

    fix: middleware.go

--- auth_service/internal/delivery/middleware/middleware.go
+++ auth_service/internal/delivery/middleware/middleware.go
@@ -16,14 +16,14 @@ func CheckAuthorization(tm *managers.TokenManager, logger *customLogger.Logger) 
 
 		if len(authHeader) == 0 {
 			logger.ErrorLogger.Error().Msg("authorization token is absent")
-			ctx.Writer.WriteHeader(http.StatusUnauthorized)
+			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 
 		bearerToken := strings.Split(authHeader, " ")
 		if bearerToken[0] != "Bearer" || len(bearerToken) != 2 {
 			logger.ErrorLogger.Error().Msg("invalid format of auth token")
-			ctx.Writer.WriteHeader(http.StatusUnauthorized)
+			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 
@@ -31,32 +31,32 @@ func CheckAuthorization(tm *managers.TokenManager, logger *customLogger.Logger) 
 		accessToken, err := tm.ValidateAccessToken(tokenString)
 		if err != nil {
 			logger.ErrorLogger.Error().Msg(err.Error())
-			ctx.Writer.WriteHeader(http.StatusBadRequest)
+			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 
 		claims, ok := accessToken.Claims.(jwt.MapClaims)
 		if !ok {
 			logger.ErrorLogger.Error().Msg("invalid format of auth token")
-			ctx.Writer.WriteHeader(http.StatusUnauthorized)
+			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 		userID, ok := claims["sub"].(string)
 		if !ok {
 			logger.ErrorLogger.Error().Msg("unable to get `sub` claim from token")
-			ctx.Writer.WriteHeader(http.StatusUnauthorized)
+			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 		exp, ok := claims["exp"].(float64)
 		if !ok {
 			logger.ErrorLogger.Error().Msg("unable to get `ip` claim from token")
-			ctx.Writer.WriteHeader(http.StatusUnauthorized)
+			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 		currentTime := time.Now().Unix()
 		if currentTime > int64(exp) {
 			logger.ErrorLogger.Error().Msg("token is expired")
-			ctx.Writer.WriteHeader(http.StatusUnauthorized)
+			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 		ctx.Set("userID", userID)
